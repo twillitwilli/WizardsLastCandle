6c6039fba619692a4dd7526ff3cb38e948ffe7b3
unknown <eclipsek24@gmail.com>
2025-08-18 15:50:56 -0500

    Added Unity Input Controls

--- Assets/Scripts/PlayerScipts/PlayerController.cs
+++ Assets/Scripts/PlayerScipts/PlayerController.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void Movement(Vector2 movement)
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
